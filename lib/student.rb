695191b24081d34bad764ebbe73cd0e84520fe20
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-03 19:07:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
     SQL
-    
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
@@ -67,7 +67,8 @@ class Student
       SELECT * FROM students WHERE grade = 9
     SQL
 
-
-
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
