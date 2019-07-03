aa4412bc0d7a7a0d12dd7c55fbc0ad423619e04d
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-03 19:02:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,8 +28,8 @@ class Student
       SELECT * FROM students
       WHERE name = ?
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
     end.first
   end
 
