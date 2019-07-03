77db033e2ccd2e94be689b8c2888dbcba05109a9
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-03 19:14:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,7 +84,7 @@ class Student
 
   def self.first_x_students_in_grade_10(x)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
 
     DB[:conn].execute(sql).map do |row|
