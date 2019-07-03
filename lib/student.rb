77d2fce6ea51921f2f9cf6ca3cdde72937a114c7
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-03 18:56:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,12 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def inicialize(name, grade, id=nil)
-    @name = name
-    @grade = grade
-    @id = id
-    save
-  end
-
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    student = Student.new(name, grade)
-    student.save
+    student = Student.new(row)
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
     student
   end
 
